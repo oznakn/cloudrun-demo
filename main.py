32252b61e1bfad169958ef12c7357e9497c017a8
Ozan Akın <ozn.akn@gmail.com>
2021-04-04 14:59:01 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/ozan')
+def hello_ozan():
+    return 'Hello Ozan!'
+
+@app.route('/click')
+def hello_click():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 6000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
